ee291146806d006343efebcced4a70942ad0c3e8
Andrei <andrey.ausianik@gmail.com>
2021-08-03 12:06:41 +0300

    fix cors

--- src/functions/createProduct/index.ts
+++ src/functions/createProduct/index.ts
@@ -1,4 +1,5 @@
 import { handlerPath } from '@libs/handlerResolver';
+import schema from "./schema";
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
@@ -7,6 +8,16 @@ export default {
       http: {
         method: 'post',
         path: 'products',
+        cors: {
+          origins: "*",
+          headers: "*",
+          methods: ["POST", "OPTIONS"],
+        },
+        request: {
+          schema: {
+            "application/json": schema
+          }
+        }
       }
     }
   ]
